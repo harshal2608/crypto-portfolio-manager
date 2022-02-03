a719bbd87a6f92674a0697bbcaf271801497e7eb
Harshal <harshalmaniya26@gmail.com>
2022-02-03 18:05:21 +0530

    bug url not valid-3

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://pro-api.coinmarketcap.com",
   headers: {
-    "X-CMC_PRO_API_KEY": import.meta.env.VITE_API_KEY,
+    "X-CMC_PRO_API_KEY": "3297bbec-def4-46ef-80cb-301d24c4bfd4",
     Accept: "application/json",
     mode: "cors",
     // "Accept-Encoding": "deflate,gzip",
